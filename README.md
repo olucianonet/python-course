f5e8e756231e091db244977992cfb4b413901f32
Luciano <oluciano.net@gmail.com>
2020-06-06 10:24:40 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,11 @@
+# Python Course
+
+Notas de estudos sobre Python
+
+## 2. Herança
+
+Tópicos estudados:
+
+- Herança
+- Generalização/especialização
+- Método super()
---
+++ src/modelo.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def likes(self):
+        return self._likes
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    def dar_like(self):
+        self._likes += 1
+
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
+
+
+class NoConstr:
+    def hello(self):
+        print('Hello!')
